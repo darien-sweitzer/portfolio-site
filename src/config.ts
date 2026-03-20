103ff0d5d1e3f6b57aee1d03586e553333ac1b33
darien-sweitzer <43593384+darien-sweitzer@users.noreply.github.com>
2026-03-20 18:13:49 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/config.ts
@@ -0,0 +1,80 @@
+export const siteConfig = {
+  name: "Darien Sweitzer",
+  title: "IT Infrastructure Specialist in Healthcare | Full Stack Developer in the Making",
+  description: "Portfolio website of Darien Sweitzer",
+  accentColor: "#7520ff",
+  social: {
+    email: "sweitzerdarien@gmail.com",
+    github: "https://github.com/darien-sweitzer",
+  },
+  aboutMe:
+    "I am a 3+ year Senior IT Specialist Technician at the 2nd largest hospital in Pittsburgh, Allegheny General Hospital. I am also a graduate of Software Engineering that's very excited to start my career and would love to do that with a company that actually makes a difference in the world. I am a huge team player that is willing to make sacrifices for the greater good of my peers while also being self-sufficient in my duties. Goal-oriented, self driven, and highly focused on moving forward, I know that I could be of great benefit to any company and a great fit into the culture of your team!",
+  skills: ["Java", "Git", "SDLC", "Javascript", "React", "Node.js", "Python", "AWS"],
+  projects: [
+    {
+      name: "URL Shortener Web App",
+      description:
+        "Full-stack web app that generates short links with analytics tracking. Built with React, Firebase, Python Flask, and Heroku.",
+      link: "https://dees-ez-links-fa9d53fd4b10.herokuapp.com/app",
+      skills: ["React", "Node.js", "Python", "Firebase", "Heroku"],
+    },
+    {
+      name: "URL Shortener Web App",
+      description:
+        "Full-stack web app that generates short links with analytics tracking. Built with React, Firebase, Python Flask, and Heroku.",
+      link: "https://dees-ez-links-fa9d53fd4b10.herokuapp.com/app",
+      skills: ["React", "Node.js", "Python", "Firebase", "Heroku"],
+    },
+    {
+      name: "URL Shortener Web App",
+      description:
+        "Full-stack web app that generates short links with analytics tracking. Built with React, Firebase, Python Flask, and Heroku.",
+      link: "https://dees-ez-links-fa9d53fd4b10.herokuapp.com/app",
+      skills: ["React", "Node.js", "Python", "Firebase", "Heroku"],
+    },
+  ],
+  experience: [
+    {
+      company: "Highmark",
+      title: "IT Infrastructure Specialist",
+      dateRange: "May 2023 - Present",
+      bullets: [
+        "Managed large projects to deploy all new hardware & software in hospital departments from scratch",
+        "Collaborated with seperate teams to ensure seamless integration of new technologies",
+        "Used Communication skills to work with new users everyday to troubleshoot and resolve technical issues",
+      ],
+    },
+    {
+      company: "Posture Construction",
+      title: "Construction Worker",
+      dateRange: "Jan 2022",
+      bullets: [
+        "Learned the importance of hard work & dedication while working 50+ hour weeks in a physically demanding job",
+        "Developed valuable skills in teamwork, communication, & problem-solving while working on a construction site",
+        "Made sacrifices for the greater good of my team, even if it meant taking on extra work for myself",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Community College of Allegheny County",
+      degree: "Associate of Science in Computer Science | Software Engineering",
+      dateRange: "2019 - 2021",
+      achievements: [
+        "A+'s in all programming courses",
+        "Graduated with 3.5 GPA",
+        "Tutor for advanced Java",
+      ],
+    },
+    {
+      school: "Free Code Camp .com",
+      degree: "Full Stack Development Certificate",
+      dateRange: "2026",
+      achievements: [
+        "Completed 600+ hours of coursework",
+        "Built 4+ portfolio projects",
+        "Specialized in React and Node.js",
+      ],
+    },
+  ],
+};
